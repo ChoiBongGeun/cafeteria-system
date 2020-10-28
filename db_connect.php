0852bfbd7c9f038aeccb8824e0ccb4de67dff2f1
choibonggeun <dhy00085@naver.com>
2020-10-28 10:10:22 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 	putenv("NLS_LANG=KOREAN_KOREA.KO16MSWIN949"); // 한글 깨짐 현상 해결
     //DB 연결할 주소 설정
-	$db = "(DESCRIPTION =(ADDRESS =(PROTOCOL = TCP)(HOST =203.249.87.162)(PORT = 1521))(CONNECT_DATA = (SID = orcl)))";
+	$db = "";
     //Env("ORACLE_HOME=/oracle/app/oracle/product/10.2.0");
 	//DB 연결
 	$id = "아이디입력";
